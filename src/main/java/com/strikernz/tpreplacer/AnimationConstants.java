cff7c125ad105332c350e394b28aa49d04acb835
STRIKERnz <Samb231@gmail.com>
2026-03-11 08:57:10 +1300

    Refactor teleport animation ID

--- src/main/java/com/strikernz/tpreplacer/AnimationConstants.java
+++ src/main/java/com/strikernz/tpreplacer/AnimationConstants.java
@@ -1,8 +1,5 @@
 package com.strikernz.tpreplacer;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Animation, graphic, and sound-effect IDs for all supported teleport types.
  */
@@ -52,32 +49,10 @@ public final class AnimationConstants
 	public static final int GIANTSOUL_AMULET_TELEPORT_GRAPHIC = 3226;
 	public static final int GIANTSOUL_AMULET_TELEPORT_SOUND = 10193;
 
-	/**
-	 * All known teleport animation IDs. This set is built from constants in this
-	 * class rather than referencing {@link TeleportAnimation} to avoid a circular
-	 * static-initialisation dependency that would cause a NullPointerException.
-	 */
-	private static final Set<Integer> TELEPORT_ANIMATION_IDS = new HashSet<>();
-
-	static
-	{
-		TELEPORT_ANIMATION_IDS.add(COWBELL_TELEPORT);
-		TELEPORT_ANIMATION_IDS.add(STANDARD_AND_JEWELLERY_TELEPORT);
-		TELEPORT_ANIMATION_IDS.add(ANCIENT_TELEPORT);
-		TELEPORT_ANIMATION_IDS.add(ARCEUUS_TELEPORT);
-		TELEPORT_ANIMATION_IDS.add(LUNAR_TELEPORT);
-		TELEPORT_ANIMATION_IDS.add(TAB_TELEPORT);
-		TELEPORT_ANIMATION_IDS.add(TELEPORT_SCROLLS);
-		TELEPORT_ANIMATION_IDS.add(ECTOPHIAL_TELEPORT);
-		TELEPORT_ANIMATION_IDS.add(ARDOUGNE_TELEPORT);
-		TELEPORT_ANIMATION_IDS.add(DESERT_AMULET_TELEPORT);
-		TELEPORT_ANIMATION_IDS.add(GIANTSOUL_AMULET_TELEPORT);
-	}
-
 	private AnimationConstants() {}
 
 	public static boolean isTeleportAnimation(int animationId)
 	{
-		return TELEPORT_ANIMATION_IDS.contains(animationId);
+		return TeleportAnimation.fromAnimationId(animationId) != null;
 	}
 }
